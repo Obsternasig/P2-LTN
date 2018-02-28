ceff87fa9e58636094f43aa9f6e266bfe401f5b0
Obsternasig <36475440+Obsternasig@users.noreply.github.com>
2018-02-28 12:07:50 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="LTN.css">
+	<title>Lan Team Nord</title>
+</head>
+
+<body>
+	<div class="grid">
+			<div class="box1"><input maxlength="1" size="1" autofocus></div>
+			<div class="box2"><input maxlength="1" size="1"></div>
+			<div class="box3"><input maxlength="1" size="1"></div>
+			<div class="box4"><input maxlength="1" size="1"></div>
+	</div>
+</body>
+</html>
