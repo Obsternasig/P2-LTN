f6db9e2d5483d49cbf9466f0b57600bb0e40dc28
Blendstrup <blendstrup@users.noreply.github.com>
2018-02-28 12:53:54 +0100

    Tilføjet DIV igen

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 			<div class="box1"><input maxlength="1" size="1" autofocus></div>
 			<div class="box2"><input maxlength="1" size="1"></div>
 			<div class="box3"><input maxlength="1" size="1"></div>
+			<div class="box4"><input maxlength="1" size="1"></div>
 	</div>
 </body>
 </html>
