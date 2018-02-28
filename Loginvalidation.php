853b5e9392a9918f45b4763025e102e309475295
Moelbak <MarcusAndersen@live.dk>
2018-02-28 13:01:15 +0100

    login

---
+++ Loginvalidation.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+	<form action="welcome.php" method="post">
+	Name: <input type="text" name="name"> <br>
+	E-mail: <input type="text" name="email"><br>
+	<input type="submit">
+	</form>
+?>	
+</body>
+</html>
