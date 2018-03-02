793891f3986ea4c7301c3f7121c1c8800281e7f7
Blendstrup <blendstrup@users.noreply.github.com>
2018-03-02 19:23:11 +0100

    Ændret sikkerhed ift database og input

--- Testing/signup-inc.php
+++ Testing/signup-inc.php
@@ -4,16 +4,16 @@ require_once '../dbconnection.php';
 
 	if(isset($_POST['first'])&&isset($_POST['last'])&&isset($_POST['email'])&&isset($_POST['uid'])&&isset($_POST['pwd'])){
 				
-				$first = htmlentities($_POST['first']);
-				$last = htmlentities($_POST['last']);
-				$email = htmlentities($_POST['email']);
-				$uid = htmlentities($_POST['uid']);
-				$pwd = htmlentities($_POST['pwd']);
+				$first = mysql_real_escape_string($_POST["first"]);
+				$last = mysql_real_escape_string($_POST["last"]);
+				$email = mysql_real_escape_string($_POST["email"]);
+				$uid = mysql_real_escape_string($_POST["uid"]);
+				$pwd = mysql_real_escape_string($_POST["pwd"]);
 
 
 		if(!empty($first)&&!empty($last)){
 
-			$query = "INSERT INTO users VALUES ('', $first', '$last', '$email', '$uid', '$pwd')";
+			$query = "INSERT INTO users (user_first, user_last, user_email, user_uid, user_pwd) VALUES ($first', '$last', '$email', '$uid', '$pwd')";
 			$results = mysqli_query($connection, $query);
 
 			if(!$results){
