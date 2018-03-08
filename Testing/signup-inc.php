1cd9fb5e9dca0d5e92c30469b1b45793e47facd2
Moelbak <MarcusAndersen@live.dk>
2018-03-08 14:54:18 +0100

    Un-merged

--- Testing/signup-inc.php
+++ Testing/signup-inc.php
@@ -8,28 +8,6 @@ require_once '../dbconnection.php';
 				$last = htmlentities($_POST['last']);
 				$email = htmlentities($_POST['email']);
 	
-
-<<<<<<< HEAD
-		/* function generatePIN($digits = 4){
-    		$i = 0; //counter
-			$pin = ""; //Default pin is blank.
-				
-				while($i < $digits){
-					//Generates a random number between 0 and 9.
-					$pin .= mt_rand(0, 9);
-					$i++;
-				}
-			return $pin;
-		}
-		
-		$pin = generatePIN(4);
-		*/
-		
-		if(!empty($first)&&!empty($last)&&!empty($email)&&!empty($pin)){
-
-			$query = "INSERT INTO users (user_first, user_last, user_email) VALUES ('', '$first', '$last', '$email', '')";
-=======
-		
 		function generatePIN($digits = 4){
 				$i = 0;
 				$pin = "";
@@ -41,13 +19,12 @@ require_once '../dbconnection.php';
 			return $pin;
 		}
  
-		$pin = generatePIN();
+		$pin = generatePIN(4);
 		
 		
 		if(!empty($first)&&!empty($last)&&!empty($email)){
 
 			$query = "INSERT INTO users (ltn_pin, user_first, user_last, user_email) VALUES ('$pin', '$first', '$last', '$email')";
->>>>>>> 8e443561e791cbb0e4b7aaf414081295e5de929f
 			$results = mysqli_query($connection, $query);
 
 			if(!$results){
