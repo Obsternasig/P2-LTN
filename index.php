e869eaa9c7aa72b75b09821da7c28b742787e1d7
Moelbak <MarcusAndersen@live.dk>
2018-03-13 14:09:23 +0100

    Two extra boxes - Initials

--- index.php
+++ index.php
@@ -10,8 +10,12 @@
 	<div class="grid">
 			<div class="box1"><input onkeyup="autoTab(this)" maxlength="1" size="1" autofocus></div>
 			<div class="box2"><input onkeyup="autoTab(this)" maxlength="1" size="1"></div>
+			<div class="dash"><p>&#x2015;</p></div>
 			<div class="box3"><input onkeyup="autoTab(this)" maxlength="1" size="1"></div>
 			<div class="box4"><input onkeyup="autoTab(this)" maxlength="1" size="1"></div>
+			<div class="box5"><input onkeyup="autoTab(this)" maxlength="1" size="1"></div>
+			<div class="box6"><input onkeyup="autoTab(this)" maxlength="1" size="1"></div>
+			
 	</div>
 
     <script type="text/javascript">
