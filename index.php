b8e8fe9cdc3d8e1cfb2a0a83e28af00060846190
Moelbak <MarcusAndersen@live.dk>
2018-03-16 15:16:10 +0100

    Merge changes

--- index.php
+++ index.php
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-<!doctype html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" href="LTN.css">
-	<title>Lan Team Nord</title>
-</head>
-
-<body>
-<form id="login" method="post" action="loginvalidation.php">
-	<div class="grid">
-			<div class="box1"><input onkeyup="autoTab(this)" maxlength="1" size="1" autofocus></div>
-			<div class="box2"><input onkeyup="autoTab(this)" maxlength="1" size="1"></div>
-			<div class="dash"><p>&#x2015;</p></div>
-			<div class="box3"><input onkeyup="autoTab(this)" maxlength="1" size="1"></div>
-			<div class="box4"><input onkeyup="autoTab(this)" maxlength="1" size="1"></div>
-			<div class="box5"><input onkeyup="autoTab(this)" maxlength="1" size="1"></div>
-			<div class="box6"><input onkeyup="autoTab(this)" maxlength="1" size="1"></div>
-	</div>
-</form>
-
-    <script type="text/javascript">
-        var x;
-        function autoTab( obj ) {
-            x = obj;
-
-            if ( obj.value.length >= obj.maxLength && obj.parentElement.nextElementSibling )
-                obj.parentElement.nextElementSibling.firstElementChild.focus();
-        }
-    </script>
-	
-	
-</body>
-=======
 <!doctype html>
 <html>
 <head>
@@ -76,5 +41,4 @@
         }
     </script>
 </body>
->>>>>>> 7c4800e848497211be18056863c0e51ec3d3861e
 </html>
