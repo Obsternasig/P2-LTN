94667801d7e85cf063ca9b8ba155b7f0e67b4ba1
Moelbak <MarcusAndersen@live.dk>
2018-03-19 10:33:42 +0100

    Loginvalidation

--- loginvalidation.php
+++ loginvalidation.php
@@ -7,24 +7,29 @@
 
 <body>
 <?php
+	
 	require_once 'dbconnection.php';
 	
-	function login($initials, $pin) {
-		$ini_pin = sha1($pin);
-    	$result = mysqli_query($connection, "SELECT * FROM users WHERE initials='$initials' AND 	ltn_pin='$pin'");
-    while($row = mysqli_fetch_array($result)) {
-        $success = true;
-		}
-    if($success == true) {
-        $_SESSION['initials']= $initials; 
-        //redirect to home page
-    } else {
-        echo '<div class="alert alert-danger">Oops! It looks like your initials and/or pincode are incorrect. Please try again.</div>';
-    }
-		/*
-		header('location: database')
-		*/
-} // END LOGIN FUNCTION
+   
+	if(isset($_POST["name"], $_POST["password"])) 
+    {     
+
+        $name = $_POST["name"]; 
+        $password = $_POST["password"]; 
+
+        $result1 = mysql_query("SELECT username, password FROM Users WHERE username = '".$name."' AND  password = '".$password."'");
+
+        if(mysql_num_rows($result1) > 0 )
+        { 
+            $_SESSION["logged_in"] = true;
+			$_SESSION["naam"] = $name; 
+        }
+        else
+        {
+            echo 'The username or password are incorrect!';
+        }
+	}
+ 
 ?>	
 </body>
 </html>
