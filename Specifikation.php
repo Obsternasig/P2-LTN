60372d5a23ced30a5e5b6b03145d79c952aaaae3
DanyHH <dany.huynh1996@yahoo.com>
2018-03-22 13:35:07 +0100

    Indtast af specifikation

--- Specifikation.php
+++ Specifikation.php
@@ -2,36 +2,33 @@
 
 <head>
 <meta charset="UTF-8">
-<link rel="stylesheet" href="Tilføjknap.css">
+<link rel="stylesheet" href="tilfoej.css">
 <title>Tilføj komponent</title>
 </head>
 	
 <body>	
-		
-
-
-	<form name="tilføj" id="til'føj" method="post" autocomplete="on">
-		<div class="specifikation">
+	<form name="form" id="form" method="post" autocomplete="on" action="specifikationdatabase.php">
+		<div>
 			<p>Navn:</p>
 			<input type="text" name="navn" id="navn" required size="30" maxlength="30">
 		</div>
 
-		<div class="specifikation">
+		<div>
 			<p>Lokation:</p>
 			<input type="text" name="lokation" id="lokation" required size="30" maxlength="30">
 		</div>
 
-		<div class="specifikation">
+		<div>
 			<p>Antal:</p>
 			<input type="number" name="antal" id="antal" required size="10" maxlength="10">
 		</div>
 
-		<div class="specifikation">
+		<div>
 			<p>Beskrivelse:</p>
 			<input type="text" name="beskrivelse" id="beskrivelse" required size="50" maxlength="50">
 		</div>
 
-		<div class="specifikation">
+		<div>
 			<input type="submit" id="ok" value="OK">
 			<input type="reset" id="reset" value="Reset">
 		</div>
--- specifikationconnection.php
+++ specifikationconnection.php
@@ -1,5 +1,5 @@
 <?php
-	$connection = mysqli_connect('localhost','root','','tilføj');
+	$connection = mysqli_connect('localhost','root', '','komponent');
 
 	if(!$connection){
 		die("Cannot connect to database".mysqli_connect_error());
