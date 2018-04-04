c71c306731a4c7868b30a97a033c9abadad540bc
Blendstrup <blendstrup@users.noreply.github.com>
2018-04-04 11:02:31 +0200

    Hvad er webkit?

--- Blendstrup/connection.php
+++ Blendstrup/connection.php
@@ -1,6 +1,6 @@
 <?php
 //database connection
-$connection = mysqli_connect('localhost','root','','komponenter');
+$connection = mysqli_connect('localhost','root','','4ndy_dk');
 
 	if(!$connection){
 		die("Could not connect to the database".mysqli_connect_error());
