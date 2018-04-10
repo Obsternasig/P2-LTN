86556018145c7218945c693b1b5c9f846d57459e
Blendstrup <blendstrup@users.noreply.github.com>
2018-04-10 10:23:31 +0200

    Denno lugter

--- Blendstrup/login.php
+++ Blendstrup/login.php
@@ -3,23 +3,44 @@
 <head>
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="adaptivegrid.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<title> Blendstrup </title>
 </head>
 
 <body>
 	<div class="pingrid">
-		<form name="pincheck" id="pincheck" method="post" action="loginvalidation.php">
-			<input class="interactive" id="ini1" name="ini1" onkeyup="autoTab(this)" maxlength="1" autocomplete="off" autofocus>
-			<input class="interactive" id="ini2" name="ini2" onkeyup="autoTab(this)" maxlength="1" autocomplete="off">
-            <div class="dash">&#x2015;</div>
-			<input class="interactive" id="pin1" name="pin1" onkeyup="autoTab(this)" maxlength="1" autocomplete="off">
-			<input class="interactive" id="pin2" name="pin2" onkeyup="autoTab(this)" maxlength="1" autocomplete="off">
-			<input class="interactive" id="pin3" name="pin3" onkeyup="autoTab(this)" maxlength="1" autocomplete="off">
-			<input class="interactive" id="pin4" name="pin4" onkeyup="autoTab(this)" maxlength="1" autocomplete="off">
+		<form name="pincheck" id="pincheck" method="post" action="loginvalidation.php" autocomplete="off">
+			<input class="interactive" id="ini1" name="ini1" maxlength="1" autofocus>
+			<input class="interactive" id="ini2" name="ini2" maxlength="1">
+			<input class="interactive" id="pin1" name="pin1" maxlength="1">
+			<input class="interactive" id="pin2" name="pin2" maxlength="1">
+			<input class="interactive" id="pin3" name="pin3" maxlength="1">
+			<input class="interactive" id="pin4" name="pin4" maxlength="1">
 		</form>
 	</div>
 
     <script>
+		$("document").ready(function(){
+			
+			$('input').keyup(function () {
+				
+				if (this.value.length == this.maxLength) {
+					$(this).next('input').focus();
+					
+				} else if (this.value.length == 0) {
+					
+					$(this).prev('input').focus();
+				}
+			});
+
+			
+			$(document).on('change', '#pin4', function() {
+   				
+				$("#pincheck").submit();
+				
+			});
+		});
+		/*
         function autoTab( obj ) {
             if ( obj.value.length >= obj.maxLength && obj.nextElementSibling ) {
                 nextElementSiblingInput(obj).focus();
@@ -41,6 +62,7 @@
             } while (element.nodeName !== "INPUT");
             return element;
         }
+		*/
     </script>
 </body>
 </html>
