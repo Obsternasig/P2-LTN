313008787796cd519d3540abec184701fb05f9b6
D3nn0 <37832243+D3nn0@users.noreply.github.com>
2018-04-26 10:48:17 +0200

    css

--- Denno/getinfo.php
+++ Denno/getinfo.php
@@ -19,31 +19,29 @@ require_once "connection.php";
 	
 	echo "<h2 class='infotekst'>Information</h2>";
 
-	echo "<button class='interactive b'> Rediger Enhed </button>";
-
 
 	if(isset($id)) {
 		$komp = mysqli_query($connection, "SELECT * FROM komponenter WHERE ID LIKE '" . $id . "' GROUP BY category, brand, ports");
 		$kompassoc = mysqli_fetch_assoc($komp);
 		
-		echo "<h3> Kategori: " . "</h3>";
-		echo ucfirst($kompassoc['category']);
+		echo "<h3 class='infoover'> Kategori: " . "</h3>";
+		echo "<p class='infoinfo'>" . ucfirst($kompassoc['category']) . "</p>";
 		
-		echo "<h3> Mærke: " . "</h3>";
-		echo ucfirst($kompassoc['brand']);
+		echo "<h3 class='infoover'> Mærke: " . "</h3>";
+		echo "<p class='infoinfo'>" . ucfirst($kompassoc['brand']) . "</p>";
 		
-		echo "<h3> Placering: " . "</h3>";
-		echo ucfirst($kompassoc['location']);
+		echo "<h3 class='infoover'> Placering: " . "</h3>";
+		echo "<p class='infoinfo'>" . ucfirst($kompassoc['location']) . "</p>";
 		
-		echo "<h3> Status: " . "</h3>";
+		echo "<h3 class='infoover'> Status: " . "</h3>";
 		
 		echo "Udlånt: " . ucfirst($kompassoc['away']);
 		echo "<br>";
 		echo "Ødelagt: ";
 		echo ucfirst($kompassoc['broken']);
 		
-		echo "<h3> Kommentar: " . "</h3>";
-		echo ucfirst($kompassoc['comment']);
+		echo "<h3 class='infoover'> Kommentar: " . "</h3>";
+		echo "<p class='infoinfo'>" . ucfirst($kompassoc['comment']) . "</p>";
 		
 		
 	}
